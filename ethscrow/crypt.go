9e44bb3d1853dfad172fe48af64dbf5162c1a9ce
Ahmad Ayubi <aayub041@uottawa.ca>
2022-03-14 17:53:52 -0400

    Use S256 rather than P256 curve

--- ethscrow/crypt.go
+++ ethscrow/crypt.go
@@ -3,6 +3,7 @@ package ethscrow
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"crypto/md5"
 	"crypto/rand"
 	"crypto/x509"
 	"encoding/hex"
@@ -20,7 +21,7 @@ func encodeToHexString(arr []byte) string {
 func decodeToHex(s string) []byte {
 	arr, err := hex.DecodeString(s)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("decodeToHex: %v", err)
 		return nil
 	}
 	return arr
@@ -32,7 +33,7 @@ func GenerateKeyPair(this js.Value, args []js.Value) interface{} {
 		fmt.Println(err)
 		return nil
 	}
-	privKeyBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	privKeyBytes, err := x509.MarshalPKCS8PrivateKey(privateKey) // TODO: switch to  MarshalECPrivateKey
 	if err != nil {
 		fmt.Println(err)
 		return nil
@@ -67,7 +68,7 @@ func getPublicKey(key string) *ecdsa.PublicKey {
 
 	publicKey, err := x509.ParsePKIXPublicKey(publicKeyHex)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("getPublicKey: %v", err)
 		return nil
 	}
 
@@ -80,7 +81,7 @@ func Sign(this js.Value, args []js.Value) interface{} {
 
 	r, s, err := ecdsa.Sign(rand.Reader, privateKey, data)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Sign: %v", err)
 		return nil
 	}
 
@@ -96,7 +97,7 @@ func Encrypt(this js.Value, args []js.Value) interface{} {
 
 	k, err := rand.Int(rand.Reader, new(big.Int).SetBytes([]byte("FFFFFFFFFFFF")))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Encrypt: %v", err)
 		return nil
 	}
 
@@ -125,11 +126,22 @@ func Decrypt(this js.Value, args []js.Value) interface{} {
 }
 
 func GenerateThreshold(this js.Value, args []js.Value) interface{} {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	privateKey, err := ecdsa.GenerateKey(eth.S256(), rand.Reader)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("GenerateThreshold: %v", err)
 		return nil
 	}
+
+	privInt := new(big.Int).SetBytes(privateKey.D.Bytes())
+	xG := []int64{privateKey.PublicKey.X.Int64(), privateKey.PublicKey.Y.Int64()}
+	G := []int64{privateKey.Curve.Params().Gx.Int64(), privateKey.Curve.Params().Gy.Int64()}
+
+	v, _ := rand.Int(rand.Reader, new(big.Int).Sub(eth.S256().Params().N, new(big.Int).SetInt64(1)))
+	vx, vy := privateKey.Curve.ScalarBaseMult(v.Bytes())
+	h := md5.Sum([]byte(fmt.Sprintf("%v%v%v", G, privInt.Int64(), xG)))
+
+	c := new(big.Int).SetBytes(h[:])
+	r := new(big.Int).Mod(new(big.Int).Sub(v, new(big.Int).Mul(c, privInt)), privateKey.Curve.Params().N)
 
 	x, y := privateKey.ScalarBaseMult(privateKey.D.Bytes())
 
@@ -137,24 +149,43 @@ func GenerateThreshold(this js.Value, args []js.Value) interface{} {
 		"privateShare": encodeToHexString(privateKey.D.Bytes()),
 		"publicShareX": encodeToHexString(x.Bytes()),
 		"publicShareY": encodeToHexString(y.Bytes()),
+		"c":            encodeToHexString(c.Bytes()),
+		"r":            encodeToHexString(r.Bytes()),
+		"vGx":          encodeToHexString(vx.Bytes()),
+		"vGy":          encodeToHexString(vy.Bytes()),
 	})
 }
 
 func GenerateEscrowAddress(this js.Value, args []js.Value) interface{} {
 	aX, aY := decodeToHex(args[0].String()), decodeToHex(args[1].String())
 	bX, bY := decodeToHex(args[2].String()), decodeToHex(args[3].String())
+	//c := decodeToHex(args[4].String())
+	//r := decodeToHex(args[5].String())
+	//vGx, vGy := decodeToHex(args[6].String()), decodeToHex(args[7].String())
+	//
+	bxInt, byInt := new(big.Int).SetBytes(bX), new(big.Int).SetBytes(bY)
+	//
+	//rGx, rGy := eth.S256().ScalarBaseMult(r)
+	//cxG, cyG := eth.S256().ScalarMult(bxInt, byInt, c)
+	//vCheckx, vChecky := eth.S256().Add(rGx, rGy, cxG, cyG)
 
-	x, y := elliptic.P256().Add(
+	// TODO: Fix zkp
+	//if vCheckx.Cmp(new(big.Int).SetBytes(vGx)) != 0 && vChecky.Cmp(new(big.Int).SetBytes(vGy)) != 0 {
+	//	fmt.Println("The other user doesn't have the correct private key.")
+	//	return js.ValueOf(nil)
+	//}
+
+	x, y := eth.S256().Add(
 		new(big.Int).SetBytes(aX),
 		new(big.Int).SetBytes(aY),
-		new(big.Int).SetBytes(bX),
-		new(big.Int).SetBytes(bY),
+		bxInt,
+		byInt,
 	)
 
 	key := &ecdsa.PublicKey{
 		X:     x,
 		Y:     y,
-		Curve: elliptic.P256(),
+		Curve: eth.S256(),
 	}
 
 	return js.ValueOf(eth.PubkeyToAddress(*key).Hex())
@@ -162,7 +193,7 @@ func GenerateEscrowAddress(this js.Value, args []js.Value) interface{} {
 
 func GenerateEscrowPrivateKey(this js.Value, args []js.Value) interface{} {
 	aD, bD := decodeToHex(args[0].String()), decodeToHex(args[1].String())
-	private := new(big.Int).Add(new(big.Int).SetBytes(aD), new(big.Int).SetBytes(bD))
+	private := new(big.Int).Mod(new(big.Int).Add(new(big.Int).SetBytes(aD), new(big.Int).SetBytes(bD)), eth.S256().Params().N)
 
 	return js.ValueOf(encodeToHexString(private.Bytes()))
 }
@@ -171,22 +202,27 @@ func SignEthTx(this js.Value, args []js.Value) interface{} {
 	tx := new(types.Transaction)
 	err := tx.UnmarshalJSON([]byte(args[0].String()))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("SignEthTx-unmarshal: %v", err)
 		return nil
 	}
 
-	privateKey, _ := eth.HexToECDSA(args[2].String())
-
-	//&ecdsa.PrivateKey{
-	//	D: new(big.Int).SetBytes(decodeToHex(args[2].String())),
-	//}
+	privateKey, err := eth.HexToECDSA(args[2].String())
+	if err != nil {
+		fmt.Printf("SignEthTx-HexToECDSA: %v", err)
+		return nil
+	}
 
 	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(new(big.Int).SetInt64(int64(args[1].Int()))), privateKey)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("SignEthTx-signing: %v", err)
 		return nil
 	}
+
 	signedTxJson, err := signedTx.MarshalJSON()
+	if err != nil {
+		fmt.Println("rlp encoding error.")
+		return nil
+	}
 
 	return js.ValueOf(string(signedTxJson))
 }
